d46ee66edcf5e5a6fc6ff3a462dfad2d324711e7
likelion-jisu <jisu2523@daum.net>
2020-04-21 21:43:17 +0900

    git 설정

---
+++ admin/src/test/java/com/msap/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msap.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
