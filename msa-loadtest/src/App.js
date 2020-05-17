93c7739af4f42036d3a839ceca43762a53ed3b0c
likelion-jisu <jisu2523@daum.net>
2020-05-17 17:53:17 +0900

    spring boot 연동 예제 코드

--- msa-loadtest/src/App.js
+++ msa-loadtest/src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
 import Header from './components/common/Header';
 import TabList from './components/common/TabList';
+import './App.css';
+import customAxios from './customAxios';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  console.log("HI APP");
+  // IP주소 변수 선언
+  const [ip, setIp] = useState('');
+  
+  // IP주소 값을 설정합니다.
+  function callback(data) {
+    console.log(data);
+    setIp(data);
+  }
+
+  // 첫번째 렌더링을 다 마친 후 실행합니다.
+  useEffect(
+    () => {
+      // 클라이언트의 IP주소를 알아내는 백엔드의 함수를 호출합니다.
+    console.log("HI");
+      customAxios('/ip', callback);
+      console.log("BYE");
+    }, []
+  );
+
   return (
     <div>
       {' '}
       <Header />
+      이 기기의 IP주소는 {ip}입니다.
       <TabList></TabList>
       {''}{' '}
     </div>
