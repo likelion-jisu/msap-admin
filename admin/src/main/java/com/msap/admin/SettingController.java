e5ff9817ddee729648a496730c050060a91697c0
likelion-jisu <jisu2523@daum.net>
2020-05-17 17:45:39 +0900

    CORS 문제 해

--- admin/src/main/java/com/msap/admin/SettingController.java
+++ admin/src/main/java/com/msap/admin/SettingController.java
@@ -3,17 +3,20 @@ package com.msap.admin;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 @RequestMapping("/api")
 public class SettingController {
 	@PostMapping("/ip")
 	public ResponseEntity<String> ip (HttpServletRequest request) {
 		// 요청을 보낸 클라이언트의 IP주소를 반환합니다.
+		System.out.println(request.getRemoteAddr());
 		return ResponseEntity.ok(request.getRemoteAddr());
 	}
 	
