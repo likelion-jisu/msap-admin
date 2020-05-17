9a1b8ae7f0d5d2a96484ce1b96effc6801231e66
likelion-jisu <jisu2523@daum.net>
2020-05-17 15:58:31 +0900

    react 연동 예시

--- admin/src/main/java/com/msap/admin/SettingController.java
+++ admin/src/main/java/com/msap/admin/SettingController.java
@@ -1,10 +1,22 @@
 package com.msap.admin;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class SettingController {
+	@PostMapping("/ip")
+	public ResponseEntity<String> ip (HttpServletRequest request) {
+		// 요청을 보낸 클라이언트의 IP주소를 반환합니다.
+		return ResponseEntity.ok(request.getRemoteAddr());
+	}
+	
 	@GetMapping("/setting")
 	public String getSetting() {
 		return "안녕하세요. setting 페이지입니다.";
